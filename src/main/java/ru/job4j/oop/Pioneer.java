1817086e97a00e8079302068e3bf1de196cbe235
Egor <yegeraskin13@gmail.com>
2020-03-13 01:55:52 +0400

    1.6. Взаимодействие объектов.[#246345](fix1)

--- src/main/java/ru/job4j/oop/Pioneer.java
+++ src/main/java/ru/job4j/oop/Pioneer.java
@@ -2,5 +2,6 @@ package ru.job4j.oop;
 
 public class Pioneer {
     public void kill(Wolf wolf) {
+        System.out.println("Pioneer kill Wolf");
     }
 }
--- src/main/java/ru/job4j/oop/Wolf.java
+++ src/main/java/ru/job4j/oop/Wolf.java
@@ -2,6 +2,7 @@ package ru.job4j.oop;
 
 public class Wolf {
     public void eat(Girl girl) {
+        System.out.println("Wolf eat Girl");
     }
 
     public void tryEat(Ball ball) {
