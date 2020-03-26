6b340f4b92bd40274229a53a5e4a91ccde39882a
Egor <yegeraskin13@gmail.com>
2020-03-26 16:43:00 +0400

    3. Компаратор для строк[#246265](fix1)

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,31 +5,14 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        int result = -1;
-        if (left == null && right == null) {
-            result = 0;
-        } else if (right == null) {
-            result = 1;
-        } else if (left != null) {
-            int indexLeft = 0;
-            int indexRight = 0;
-            while (indexLeft < left.length() &&
-                   indexRight < right.length()) {
-                if (left.charAt(indexLeft) !=
-                    right.charAt(indexRight)) {
-                    break;
-                }
-                indexLeft++;
-                indexRight++;
-            }
-            if (indexLeft == left.length() &&
-                indexRight == right.length()) {
-                result = 0;
-            } else if (indexRight == right.length()) {
-                result = 1;
-            } else if (indexLeft != left.length()) {
-                result = Character.compare(left.charAt(indexLeft),
-                                           right.charAt(indexRight));
+        int result = Integer.compare(left.length(), right.length());
+        int length = Math.min(left.length(), right.length());
+        for (int index = 0; index < length; ++index) {
+            if (left.charAt(index) !=
+                    right.charAt(index)) {
+                result = Character.compare(left.charAt(index),
+                                           right.charAt(index));
+                break;
             }
         }
         return result;
