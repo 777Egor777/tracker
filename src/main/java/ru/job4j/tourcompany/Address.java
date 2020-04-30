271419f4030d3a9fddf3a968aabd89ff56965e13
Egor <yegeraskin13@gmail.com>
2020-04-30 21:58:47 +0400

    2. Список адресов[#265799]

---
+++ src/main/java/ru/job4j/tourcompany/Address.java
@@ -0,0 +1,9 @@
+package ru.job4j.tourcompany;
+
+public class Address {
+    private String city;
+    private String street;
+    private int home;
+    private int apartment;
+
+}
---
+++ src/main/java/ru/job4j/tourcompany/Profile.java
@@ -0,0 +1,9 @@
+package ru.job4j.tourcompany;
+
+public class Profile {
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+}
