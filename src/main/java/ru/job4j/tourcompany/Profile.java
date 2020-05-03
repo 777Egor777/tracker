1e2ba2b63011d65339159661758421210519e83f
Egor <yegeraskin13@gmail.com>
2020-05-03 13:04:18 +0400

    2. Список адресов[#265799] (add tests)

--- src/main/java/ru/job4j/tourcompany/Profile.java
+++ src/main/java/ru/job4j/tourcompany/Profile.java
@@ -3,6 +3,10 @@ package ru.job4j.tourcompany;
 public class Profile {
     private Address address;
 
+    public Profile(Address address) {
+        this.address = address;
+    }
+
     public Address getAddress() {
         return address;
     }
---
+++ src/test/java/ru/job4j/tourcompany/ProfilesTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.tourcompany;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class ProfilesTest {
+
+    @Test
+    public void collectWhenEmptyList() {
+        List<Profile> profiles = List.of();
+        List<Address> result = Profiles.collect(profiles);
+        List<Address> expected = List.of();
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void collectWhenSingleProfile() {
+        List<Profile> profiles = List.of(
+                new Profile(new Address("Saratov", "Bolshaya Kazachya", 100, 50))
+        );
+        List<Address> result = Profiles.collect(profiles);
+        List<Address> expected = List.of(
+                new Address("Saratov", "Bolshaya Kazachya", 100, 50)
+        );
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void collectWhenFewProfiles() {
+        List<Profile> profiles = List.of(
+                new Profile(new Address("Saratov", "Bolshaya Kazachya", 100, 50)),
+                new Profile(new Address("Saratov", "Bolshaya Gornaya", 100, 50)),
+                new Profile(new Address("Saransk", "Moskowskaya", 36, 12))
+        );
+        List<Address> result = Profiles.collect(profiles);
+        List<Address> expected = List.of(
+                new Address("Saransk", "Moskowskaya", 36, 12),
+                new Address("Saratov", "Bolshaya Kazachya", 100, 50),
+                new Address("Saratov", "Bolshaya Gornaya", 100, 50)
+        );
+        assertThat(result, is(expected));
+    }
+}
