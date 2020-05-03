f076a44af075abbd96b3c4ec1639864a81bcf619
Egor <yegeraskin13@gmail.com>
2020-05-03 13:20:33 +0400

    3. Уникальность элементов и сортировка.[#265800] (tests)

--- src/test/java/ru/job4j/tourcompany/ProfilesTest.java
+++ src/test/java/ru/job4j/tourcompany/ProfilesTest.java
@@ -44,4 +44,21 @@ public class ProfilesTest {
         );
         assertThat(result, is(expected));
     }
+
+    @Test
+    public void collectWhenConsistEqualsProfiles() {
+        List<Profile> profiles = List.of(
+                new Profile(new Address("Saratov", "Bolshaya Kazachya", 100, 50)),
+                new Profile(new Address("Saratov", "Bolshaya Gornaya", 100, 50)),
+                new Profile(new Address("Saransk", "Moskowskaya", 36, 12)),
+                new Profile(new Address("Saransk", "Moskowskaya", 36, 12))
+        );
+        List<Address> result = Profiles.collect(profiles);
+        List<Address> expected = List.of(
+                new Address("Saransk", "Moskowskaya", 36, 12),
+                new Address("Saratov", "Bolshaya Kazachya", 100, 50),
+                new Address("Saratov", "Bolshaya Gornaya", 100, 50)
+        );
+        assertThat(result, is(expected));
+    }
 }
