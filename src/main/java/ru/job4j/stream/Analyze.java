2e079679f57a9320866e18b33ec494dc5f793569
Egor <yegeraskin13@gmail.com>
2020-09-13 14:28:56 +0400

    Крестики-нолики на JavaFX [#265811]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -1,36 +1,50 @@
 package ru.job4j.stream;
 
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Analyze {
 
     public static double averageScore(Stream<Pupil> stream) {
-        return stream.flatMap(pupil -> pupil.getMarks().stream()).mapToInt(Subject::getScore)
-                .average().orElse(0.0);
-    }
-
-    public static double averageScoreBySubject(Stream<Pupil> stream) {
-        return stream.flatMap(pupil -> pupil.getMarks().stream()).mapToInt(Subject::getScore)
-                .average().orElse(0.0);
-    }
-
-    public static double averageScoreByPupil(Stream<Pupil> stream) {
-        return stream.map(Pupil::getMarks)
-                .mapToDouble(marks -> marks.stream().mapToInt(Subject::getScore).average().orElse(0.0))
-                .average().orElse(0.0);
-    }
-
-    public static Pupil bestStudent(Stream<Pupil> stream) {
-        return stream.sorted((x, y) -> Double.compare(x.getMarks().stream().mapToInt(Subject::getScore).average().getAsDouble(),
-                                       y.getMarks().stream().mapToInt(Subject::getScore).average().getAsDouble()) * -1)
-                     .findFirst().get();
-    }
-
-    public static Subject bestSubject(Stream<Pupil> stream) {
         return stream.flatMap(pupil -> pupil.getMarks().stream())
-                     .sorted(Comparator.comparingInt(Subject::getScore).reversed())
-                     .findFirst().get();
+                .mapToInt(Subject::getScore)
+                .average().getAsDouble();
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.map(x -> new Tuple(x.getName(),
+                x.getMarks().stream().mapToInt(Subject::getScore).average().getAsDouble()))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.getMarks().stream())
+                .collect(Collectors.groupingBy(Subject::getName, HashMap::new,
+                        Collectors.mapping(Subject::getScore, Collectors.toList())))
+                .entrySet().stream().map(x -> new Tuple(x.getKey(),
+                        x.getValue().stream().mapToInt(y -> y).average().getAsDouble()))
+                .collect(Collectors.toList());
+
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(x -> new Tuple(x.getName(),
+                x.getMarks().stream().mapToInt(Subject::getScore).sum()))
+                .sorted(Comparator.comparingDouble(Tuple::getScore).reversed())
+                .findFirst().get();
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.getMarks().stream())
+                .collect(Collectors.groupingBy(Subject::getName, HashMap::new,
+                        Collectors.mapping(Subject::getScore, Collectors.toList())))
+                .entrySet().stream().map(x -> new Tuple(x.getKey(),
+                        x.getValue().stream().mapToInt(y -> y).sum()))
+                .sorted(Comparator.comparingDouble(Tuple::getScore).reversed())
+                .findFirst().get();
     }
 }
