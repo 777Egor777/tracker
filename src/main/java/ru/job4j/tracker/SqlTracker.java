f52ecbce7df1c50fa30365e96094a67e1abeb40b
Egor <yegeraskin13@gmail.com>
2020-11-29 12:55:13 +0400

    2. Трекер SQL [#292540] (fix to stable db)

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -38,7 +38,8 @@ public class SqlTracker implements Store {
                     String.format("insert into items(name) values(\'%s\');", item.getName());
             st.executeUpdate(insertQuery);
             addQueryToFile(insertQuery);
-            item.setId(""+(++currentMaxId));
+            setCurrentMaxId();
+            item.setId(""+(currentMaxId));
         } catch (Exception ex) {
             throw new IllegalStateException("Exception when adding Item", ex);
         }
@@ -49,12 +50,8 @@ public class SqlTracker implements Store {
     public boolean replace(String id, Item item) {
         boolean result = false;
         try (Statement st = cn.createStatement()) {
-            ResultSet rsCount = st.executeQuery("select count(*) from items");
-            rsCount.next();
-            int maxId = rsCount.getInt(1);
-            rsCount.close();
             int itemId = Integer.parseInt(id);
-            if (itemId <= maxId) {
+            if (idExist(itemId)) {
                 String updQuery = String.format("update items set name=\'%s\' where id=%d;", item.getName(), itemId);
                 st.executeUpdate(updQuery);
                 addQueryToFile(updQuery);
@@ -70,12 +67,8 @@ public class SqlTracker implements Store {
     public boolean delete(String id) {
         boolean result = false;
         try (Statement st = cn.createStatement()) {
-            ResultSet rsCount = st.executeQuery("select count(*) from items");
-            rsCount.next();
-            int maxId = rsCount.getInt(1);
-            rsCount.close();
             int itemId = Integer.parseInt(id);
-            if (itemId <= maxId) {
+            if (idExist(itemId)) {
                 String delQuery = String.format("delete from items where id=%d;", itemId);
                 st.executeUpdate(delQuery);
                 addQueryToFile(delQuery);
@@ -133,9 +126,6 @@ public class SqlTracker implements Store {
     @Override
     public void close() throws Exception {
         if (cn != null) {
-            try (Statement st = cn.createStatement()) {
-                st.execute("drop table items;");
-            }
             cn.close();
         }
     }
@@ -171,8 +161,8 @@ public class SqlTracker implements Store {
             if (rs.next()) {
                 result = true;
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
         }
         return result;
     }
@@ -183,5 +173,37 @@ public class SqlTracker implements Store {
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    private void setCurrentMaxId() {
+        try (Statement st = cn.createStatement()) {
+            String query = "select id from items order by id desc limit 1;";
+            try(ResultSet rs = st.executeQuery(query)) {
+                if (rs.next()) {
+                    currentMaxId = rs.getInt(1);
+                }
+            }
+
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        } finally {
+
+        }
+    }
+
+    private boolean idExist(int id) {
+        boolean result = false;
+        try (Statement st = cn.createStatement();
+             ResultSet rs = st.executeQuery(
+                     String.format(
+                             "select id from items where id=%d;",
+                             id
+                     )
+             )) {
+            result = rs.next();
+        } catch (Exception ex) {
+            throw new IllegalStateException("when id exist checking", ex);
+        }
+        return result;
     }
 }
