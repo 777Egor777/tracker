56e458e36d072262cb9bf551b8cd357aae0f7207
Egor <yegeraskin13@gmail.com>
2021-02-10 17:42:03 +0400

    0. Что такое DI? [#435400] (Refactor)

--- src/main/java/ru/job4j/di/ConsoleInput.java
+++ src/main/java/ru/job4j/di/ConsoleInput.java
@@ -2,6 +2,9 @@ package ru.job4j.di;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import ru.job4j.tracker.input.Input;
+
+import java.util.Scanner;
 
 /**
  * @author Egor Geraskin(yegeraskin13@gmail.com)
@@ -10,5 +13,85 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @Scope("prototype")
-public class ConsoleInput {
+public class ConsoleInput implements Input {
+    /**
+     * java.util.Scanner object
+     * We need this field
+     * to get input from
+     * user's keyboard
+     */
+    private final Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Method send question
+     * to standard output
+     * an input String from
+     * user
+     *
+     * @param question - we output this
+     *                 question. It need
+     *                 for user to understand
+     *                 what he should type
+     * @return String, that
+     * user type
+     */
+    @Override
+    public String askStr(String question) {
+        System.out.print(question);
+        return nextLine();
+    }
+
+    /**
+     * Method send question
+     * to standard output
+     * an input integer
+     * number from user
+     *
+     * @param question - we output this
+     *                 question. It need
+     *                 for user to understand
+     *                 what he should type
+     * @return Integer number, that
+     * user type
+     */
+    @Override
+    public int askInt(String question) {
+        return Integer.parseInt(this.askStr(question));
+    }
+
+    /**
+     * Method organize input
+     * of the integer number
+     * from user from
+     * diapason [0;max]
+     * If number not in this diapason,
+     * it trows Exception
+     * @param question - message that shows
+     *                 to user before he
+     *                 starts to type
+     * @param max      - upper bound of
+     *                 the diapason
+     * @return integer number
+     * that user enter
+     */
+    @Override
+    public int askInt(String question, int max) {
+        int result = askInt(question);
+        if (!(result >= 0 && result < max)) {
+            throw  new IllegalStateException(String.format("Number out of bounds [%d, %d)", 0, max));
+        }
+        return result;
+    }
+
+    /**
+     * Base method that
+     * return newLine
+     * from console
+     * @return new line from
+     *         console
+     */
+    @Override
+    public String nextLine() {
+        return scanner.nextLine();
+    }
 }
